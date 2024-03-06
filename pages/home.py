587027f7f207ffcd42da1f2394f5d8ee756cbfbb
Jan Lade <jan.lade@jedox.com>
2024-03-06 13:13:21 +0100

    add function descriptions

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
 def home():
+    """
+    Displaying the starting home screen.
+
+    Contains:
+    1. ETL: Extract, Transform and Load Datasets.
+    2. Wizard: Guide the user through setting up a Machine Learning Model
+    3. Dashboard: Interaction with processed Data from ETL and Wizard
+
+    Returns:
+    - None
+    """    
     st.header(":violet[S]ales Intelligence Suite", divider="violet")
     st.write("Debus Project 1.0")
     st.page_link("pages/etl.py", label="ETL", icon= "🔷")
